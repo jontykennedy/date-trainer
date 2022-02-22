12b3492ce89fc20db877960ed268fbc83b7b8b7e
Jonty Kennedy <accounts@jontykennedy.co.uk>
2022-02-22 10:42:45 +0000

    Replace string concatenation with f-strings

--- date-trainer.py
+++ date-trainer.py
@@ -16,17 +16,17 @@ def  calculate_full_year_code(full_year):
     century = full_year - year
 
     if (year == 0 and full_year % 400 == 0):
-        print("Note, " + str(full_year) + " is a leap year")
+        print(f"Note, {full_year} is a leap year")
     elif (full_year % 4 == 0):
-        print("Note, " + str(full_year) + " is a leap year")
+        print(f"Note, {full_year} is a leap year")
     else:
-        print("Note, " + str(full_year) + " is not a leap year")
+        print(f"Note, {full_year} is not a leap year")
 
     year_code = (year + (year // 4)) % 7
     century_code = calculate_century_code(century)
 
     full_year_code = year_code + century_code
-    print("Year Code: " + str(full_year_code))
+    print(f"Year Code: {full_year_code}")
 
 
 def main():
