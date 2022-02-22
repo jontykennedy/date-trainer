946048267905b252bb2f46e51f23ddba095a5f38
Jonty Kennedy <accounts@jontykennedy.co.uk>
2022-02-22 11:13:55 +0000

    Add month code calculation

--- date-trainer.py
+++ date-trainer.py
@@ -7,11 +7,14 @@ century_codes = {
     0: 0,
 }
 
+def calculate_day_code(day):
+    return day % 7
+
 def calculate_century_code(century):
     century = century % 40
     return century_codes.get(century)
 
-def  calculate_full_year_code(full_year):
+def calculate_full_year_code(full_year):
     year = full_year % 100
     century = full_year - year
 
@@ -25,13 +28,20 @@ def  calculate_full_year_code(full_year):
     year_code = (year + (year // 4)) % 7
     century_code = calculate_century_code(century)
 
-    full_year_code = year_code + century_code
-    print(f"Year Code: {full_year_code}")
+    return year_code + century_code
 
 
 def main():
-    full_year = int(sys.argv[1])
-    calculate_full_year_code(full_year)
+    date = sys.argv[1].split(".")
+    day = int(date[0])
+    month = int(date[1])
+    full_year = int(date[2])
+
+    day_code = calculate_day_code(day)
+    print(f"Day Code: {day_code}")
+
+    full_year_code = calculate_full_year_code(full_year)
+    print(f"Year Code: {full_year_code}")
     
 if __name__ == "__main__":
     main()
